1d17aa1d3d98ea2b9a368ca1d8539272eb76a06c
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2018-12-05 16:39:44 +0000

    new SGL file paths for bacon layer and hair imaging

--- USimagingPhantoms.m
+++ USimagingPhantoms.m
@@ -682,16 +682,34 @@ file_dir = '..\data\imagingUS\';
 
 %% 181204 agar-based tissue mimicking phantom (atmm) with orgasol - BK31[CNT]
 
-file_name = '181204/atmm_orgasol1_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_03s08m21h_04-12-18_avg1_2D_raw.SGL';
+% file_name = '181204/atmm_orgasol1_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_03s08m21h_04-12-18_avg1_2D_raw.SGL';
+% 
+% c0 = 1544;
+% 
+% trigger_delay = 0;
+% 
+% dim = 3;
+% 
+% samples_cut_off = 50;
+% samples_t0_correct = -6;
 
-c0 = 1544;
+
+%% 181205 bacon layers & hair - BK31[CNT]
+
+file_name = '181205/baconLayersHair_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_05s14m12h_05-12-18_avg1_2D_raw.SGL';
+
+c0 = 1540;
 
 trigger_delay = 0;
 
 dim = 3;
 
-samples_cut_off = 50;
+samples_cut_off = 0;
 samples_t0_correct = -6;
+
+
+
+
 
 
 
