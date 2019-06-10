3c1e5befdd8f1d6ff59ca9a123895617fed18522
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2019-06-10 17:03:31 +0100

    set up for line scan resolution analysis

--- USimagingRecon.m
+++ USimagingRecon.m
@@ -15,12 +15,12 @@ run('USimagingPhantoms.m')
 % for samples_t0_correct = -9
 % for c0 = 1477:1:1481
 
-% multiple file names:      -- need to add {1} to file_name below
-% scanIDs = 1:length(file_names);
-% for scanID = scanIDs(1)
-%     
-%     file_name = file_names{scanID};
-%     trigger_delay = trigger_delays{scanID};
+% multiple file names:
+scanIDs = 1:length(file_names);
+for scanID = scanIDs(1:end)
+    
+    file_name = file_names{scanID};
+    trigger_delay = trigger_delays{scanID};
 
 %% load & view SGL data
 
@@ -59,14 +59,13 @@ end
 display(['Reconstructing: ' file_name])
 
 if dim == 2
-    [reflection_image, samples_total, t_array, kgrid] = ...
-                reconstruct2dUSimage(sensor_data, params, trigger_delay, samples_cut_off, samples_t0_correct, c0);
+    [reflection_image] = reconstruct2dUSimage(sensor_data, params, c0);
 elseif dim == 3
     [reflection_image] = reconstruct3dUSimage(sensor_data, params, c0, ...
                                 'ZeroPad', 10, ...
                                 'Upsample', false, ...
                                 'Apodise', false, ...
-                                'FreqBandFilter', {10e6,15e6}, ...
+                                'FreqBandFilter', {10e6,100e6}, ...
                                 'TimeGainCompensate', {}, ...
                                 'EnvelopeDetect', true, ...
                                 'LogCompress', 0, ...
@@ -77,54 +76,56 @@ end
 
 %%
 
-sliceViewer
+% sliceViewer
 
 
 %% plot reconstructed image
 
-% figure%(1); clf(1)
-% set(gcf,'Position',[100,100,800,450])
-% switch dim
-%     case 2
-%         % imagesc(reflection_image(900:1020,160:280)')
-% %         imagesc(reflection_image(:,1:samples_total/2)')
-%         imagesc(kgrid.x_vec*1e3, t_array*c0/2*1e3, reflection_image(:,1:samples_total/2)')
-%             % 1st index (x) = row index = y axis -- transposed -> x axis
-%             xlabel('x [mm]')
-%             ylabel('z [mm]')
-%     case 3
-%         imagesc(kgrid.x_vec*1e3, t_array*c0/2*1e3, squeeze(reflection_image(:,75,1:samples_total/2))')
-%             xlabel('x [mm]')
-%             ylabel('z [mm]')
-% end
-%     title(['reconstructed image with c0 = ' num2str(c0) ', t0 correction = ' num2str(samples_t0_correct)])
-%     %axis image
-%     %caxis([0,200])
-%     cmap = colormap(gray);
-%     cmap = flipud(cmap);    % flip colormap to make black = signal
-%     colormap(cmap);
-%     colorbar
-%     %hold on
-%     %plot([0,0],[-10,10],'b--')
-%     %legend('sensor')
-%     %hold off
-% %     ylim([40,140])
-% %     xlim([1250,1450])
-% %     ylim([0.1,0.8])
-% %     xlim([0.5,2.5])
+global kgrid t_array Nt %#ok<TLEV>
+
+figure%(1); clf(1)
+set(gcf,'Position',[100,100,800,450])
+switch dim
+    case 2
+        % imagesc(reflection_image(900:1020,160:280)')
+%         imagesc(reflection_image(:,1:samples_total/2)')
+        imagesc(kgrid.x_vec*1e3, t_array*c0/2*1e3, reflection_image(:,1:Nt)')
+            % 1st index (x) = row index = y axis -- transposed -> x axis
+            xlabel('x [mm]')
+            ylabel('z [mm]')
+    case 3
+        imagesc(kgrid.x_vec*1e3, t_array*c0/2*1e3, squeeze(reflection_image(:,75,1:Nt))')
+            xlabel('x [mm]')
+            ylabel('z [mm]')
+end
+    title(['reconstructed image with c0 = ' num2str(c0) ', t0 correction = ' num2str(params.Nt_t0_correct)])
+    %axis image
+    %caxis([0,200])
+    cmap = colormap(gray);
+    cmap = flipud(cmap);    % flip colormap to make black = signal
+    colormap(cmap);
+    colorbar
+    %hold on
+    %plot([0,0],[-10,10],'b--')
+    %legend('sensor')
+    %hold off
+%     ylim([40,140])
+%     xlim([1250,1450])
+%     ylim([0.1,0.8])
+%     xlim([0.5,2.5])
 
 
 %% find image peaks and FWHM for resolution measurements
 
-% threshold = 100;
-% imagePeakFinder(reflection_image, kgrid, t_array, c0, threshold)
+threshold = 1000;
+imagePeakFinder(reflection_image, c0, threshold)
 
 
 %% run multiple reconstructions in loop (end)
 
 % pause
 
-% end     % of scanID / file_name loop
+end     % of scanID / file_name loop
 
 % end     % of c0 loop
 % end     % of samples_t0_correct loop
