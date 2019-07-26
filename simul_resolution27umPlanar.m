98351b0638b679bb752c3f5aa2186a86179270ae
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2019-07-26 17:17:44 +0100

    automate two layers simulation

--- simul_resolution27umPlanar.m
+++ simul_resolution27umPlanar.m
@@ -7,138 +7,137 @@ global kgrid t_array dt
 file_dir = 'D:\PROJECT\data\simulations\resolution27um\';
 beam_positions = {'central', 'offcentre'};
 
-for beam_pos = beam_positions(2)
+for beam_pos = beam_positions
 
     beam_pos = beam_pos{1}; %#ok<FXSET>
 
-%% SET UP EXPERIMENT
+    %% SET UP EXPERIMENT
 
-% create the computational grid
-% x_length = 6e-3;            % length of line scan [m]
-% y_length = 3e-3;            % depth of field [m]
-dx = 2e-6;                  % grid point spacing in the x direction [m]
-dy = dx;                    % grid point spacing in the y direction [m]
-Nx = 2048;    % number of grid points in the x (row) direction
-Ny = 1024;     % number of grid points in the y (column) direction
-kgrid = kWaveGrid(Nx, dx, Ny, dy);
+    % create the computational grid
+    % x_length = 6e-3;            % length of line scan [m]
+    % y_length = 3e-3;            % depth of field [m]
+    dx = 2e-6;                  % grid point spacing in the x direction [m]
+    dy = dx;                    % grid point spacing in the y direction [m]
+    Nx = 2048;    % number of grid points in the x (row) direction
+    Ny = 1024;    % number of grid points in the y (column) direction
+    kgrid = kWaveGrid(Nx, dx, Ny, dy);
 
-% define the thickness of the PML [grid points]
-pml_size = 20;
+    % define the thickness of the PML [grid points]
+    pml_size = 20;
 
-% background material properties (WATER)
-c0 = 1500;      % sound speed [m/s]
-rho0 = 1000;    % density [kg/m^3]
+    % background material properties (WATER)
+    c0 = 1500;      % sound speed [m/s]
+    rho0 = 1000;    % density [kg/m^3]
 
-% define resolution targets (TUNGSTEN)
-num_wires = 7;
-num_layers = 2;
-wire_radius = 6;                                % [grid points]
-wire_xs  = round((1:1:num_wires)*0.5e-3/dx);      % [grid points]
-wire_ys  = round((1:1:num_layers)*0.5e-3/dy);     % [grid points]
-wire_c   = 5220;                                % sound speed [m/s]
-wire_rho = 19300;                               % density [kg/m^3]
-wires_layer = zeros(Nx, Ny);
+    % define resolution targets (TUNGSTEN)
+    num_wires = 7;
+    num_layers = 2;
+    wire_radius = 6;                                                % [grid points]
+    wire_xs  = round((1:1:num_wires)*Nx/(num_wires+1));                 % [grid points]
+    wire_ys  = pml_size + round((1:1:num_layers)*0.5e-3/dy);        % [grid points]
+    wire_c   = 5220;                                % sound speed [m/s]
+    wire_rho = 19300;                               % density [kg/m^3]
+    wires_layer = zeros(Nx, Ny);
 
-% loop over diff layers of wires
-% for idx_layer = 1:num_layers
-idx_layer = 2;
-    
-% make a layer of num_wires wires
-for idx_wire = 1:num_wires
-    wire_x = wire_xs(idx_wire);
-    wire_y = wire_ys(idx_layer);
-    wires_layer = wires_layer + makeDisc(Nx, Ny, wire_x, wire_y, wire_radius);
-end
+    % loop over diff layers of wires
+    for idx_layer = 1:num_layers
 
-% define sound speed and density of medium
-medium.sound_speed = c0 * ones(Nx, Ny);          % sound speed [m/s]
-medium.density = rho0 * ones(Nx, Ny);            % density [kg/m^3]
+        % make a layer of num_wires wires
+        for idx_wire = 1:num_wires
+            wire_x = wire_xs(idx_wire);
+            wire_y = wire_ys(idx_layer);
+            wires_layer = wires_layer + makeDisc(Nx, Ny, wire_x, wire_y, wire_radius);
+        end
 
-medium.sound_speed(wires_layer==1) = wire_c;
-medium.density(wires_layer==1) = wire_rho;
+        % define sound speed and density of medium
+        medium.sound_speed = c0 * ones(Nx, Ny);          % sound speed [m/s]
+        medium.density = rho0 * ones(Nx, Ny);            % density [kg/m^3]
 
-% create the time array
-cfl = 0.2;              % CFL number
-t_end = 2*Ny*dy/c0;     % end time of the simulation [s]
-kgrid.makeTime(medium.sound_speed,cfl,t_end);
+        medium.sound_speed(wires_layer==1) = wire_c;
+        medium.density(wires_layer==1) = wire_rho;
 
-% define a planar apodised (off-central) source
-% source.p0 = zeros(Nx, Ny);
-source.p_mask = zeros(Nx, Ny);
-source.p_mask(:, pml_size + 1) = 1;
-pressure = gaussian(kgrid.t_array,1,20e-9,(5.5e-9)^2);
-sourcewidth = 0.4;
-apodisation = getWin(Nx+1000, 'Gaussian', 'Param', sourcewidth);
-switch beam_pos
-    case 'central'
-        apodisation = apodisation(501:end-500);
-    case 'offcentre'
-        apodisation = apodisation(1:end-1000);
-end
-source.p = apodisation * pressure;
-% source.p0(:, pml_size + 1) = apodisation;
+        % create the time array
+        cfl = 0.2;              % CFL number
+        t_end = 2*Ny*dy/c0;     % end time of the simulation [s]
+        kgrid.makeTime(medium.sound_speed,cfl,t_end);
 
-% define a sensor array co-aligned with the source
-sensor_positions = pml_size:10:(Nx - pml_size);
-sensor.mask = zeros(Nx, Ny);
-sensor.mask(sensor_positions,pml_size+1) = 1;
+        % define a planar apodised (off-central) source
+        % source.p0 = zeros(Nx, Ny);
+        source.p_mask = zeros(Nx, Ny);
+        source.p_mask(:, pml_size + 1) = 1;
+        pressure = gaussian(kgrid.t_array,1,20e-9,(5.5e-9)^2);
+        sourcewidth = 0.4;
+        apodisation = getWin(Nx+1000, 'Gaussian', 'Param', sourcewidth);
+        switch beam_pos
+            case 'central'
+                apodisation = apodisation(501:end-500);
+            case 'offcentre'
+                apodisation = apodisation(1:end-1000);
+        end
+        source.p = apodisation * pressure;
+        % source.p0(:, pml_size + 1) = apodisation;
 
-% calculate the number of sensor elements used
-num_sensors = sum(sensor.mask(:));
+        % define a sensor array co-aligned with the source
+        sensor_positions = pml_size:10:(Nx - pml_size);
+        sensor.mask = zeros(Nx, Ny);
+        sensor.mask(sensor_positions,pml_size+1) = 1;
 
-
-%% SIMULATE UP EXPERIMENT
-
-inputs = {'PMLSize', pml_size, 'PlotLayout', true, 'PlotSim', true};
-sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor, inputs{:});
-
-save([file_dir 'layer_' num2str(idx_layer) '_' beam_pos '_sensor_data'], 'sensor_data')
-
-figure
-imagesc(kgrid.x_vec*1e3,kgrid.t_array*1e6,sensor_data')
-    xlabel('x position / mm')
-    ylabel('time / \mus')
-    
-% end of loop over diff layers of wires
-% end
+        % calculate the number of sensor elements used
+        num_sensors = sum(sensor.mask(:));
 
 
-%% IMAGE FORMATION
+        %% SIMULATE UP EXPERIMENT
 
-params.Nx = size(sensor_data,1);
-params.Ny = 1;
-params.dx = 10*dx;
-params.dy = dy;
-params.dt = kgrid.dt;
+        inputs = {'PMLSize', pml_size, 'PlotLayout', true, 'PlotSim', true};
+        sensor_data = kspaceFirstOrder2D(kgrid, medium, source, sensor, inputs{:});
 
-params.trigger_delay        = 0;
-params.Nt_zero_pad_source   = 500;
-params.Nt_t0_correct        = -262;
-params.file_data            = ['111111\resolution27umPlanar_simul_' beam_pos];
+        save([file_dir 'layer_' num2str(idx_layer) '_' beam_pos '_sensor_data'], 'sensor_data')
 
-reflection_image = reconstruct2dUSimage(sensor_data, params, c0);
-% WARNING: kgrid UPDATED
-
-% remake kgrid time and assign to global variables used in imagePeakFinder
-kgrid.makeTime(medium.sound_speed,cfl,t_end);
-t_array = kgrid.t_array;
-dt = kgrid.dt;
-
-save([file_dir 'layer_' num2str(idx_layer) '_' beam_pos '_reflection_image'], 'reflection_image')
-
-figure
-imagesc(kgrid.x_vec*1e3,kgrid.t_array*c0*1e3/2,reflection_image')
-    axis image
-	xlabel('x position / mm')
-    ylabel('z position / mm')
+        figure
+        imagesc(kgrid.x_vec*1e3,kgrid.t_array*1e6,sensor_data')
+            xlabel('x position / mm')
+            ylabel('time / \mus')
 
 
-%% peak size analysis
+        %% IMAGE FORMATION
 
-threshold = 70;
-peaksInfo = imagePeakFinder(reflection_image, c0, threshold);
+        params.Nx = size(sensor_data,1);
+        params.Ny = 1;
+        params.dx = 10*dx;
+        params.dy = dy;
+        params.dt = kgrid.dt;
 
-save([file_dir 'layer_' num2str(idx_layer) '_' beam_pos '_peaksInfo'], 'peaksInfo')
+        params.trigger_delay        = 0;
+        params.Nt_zero_pad_source   = 500;
+        params.Nt_t0_correct        = -262;
+        params.file_data            = ['111111\resolution27umPlanar_simul_' beam_pos];
+
+        reflection_image = reconstruct2dUSimage(sensor_data, params, c0);
+        % WARNING: kgrid UPDATED
+
+        % remake kgrid time and assign to global variables used in imagePeakFinder
+        kgrid.makeTime(medium.sound_speed,cfl,t_end);
+        t_array = kgrid.t_array;
+        dt = kgrid.dt;
+
+        save([file_dir 'layer_' num2str(idx_layer) '_' beam_pos '_reflection_image'], 'reflection_image')
+
+        figure
+        imagesc(kgrid.x_vec*1e3,kgrid.t_array*c0*1e3/2,reflection_image')
+            axis image
+            xlabel('x position / mm')
+            ylabel('z position / mm')
+
+
+        %% peak size analysis
+
+        threshold = 70;
+        peaksInfo = imagePeakFinder(reflection_image, c0, threshold);
+
+        save([file_dir 'layer_' num2str(idx_layer) '_' beam_pos '_peaksInfo'], 'peaksInfo')
+
+    % end of loop over diff layers of wires
+    end
 
 
 end
