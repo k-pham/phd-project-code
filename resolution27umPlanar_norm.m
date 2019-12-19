c029e90a0a35f75acb328d0eac8864d46f41e9b3
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2019-12-19 13:37:16 +0000

    norm for 2D grid of t0 and c0 variation

--- resolution27umPlanar_norm.m
+++ resolution27umPlanar_norm.m
@@ -1,25 +1,30 @@
 
 
 file_dir = 'D:\PROJECT\figures\_Matlab figs\USimaging\191126 resolution27umPlanar BK31[CNT] trolley scrambled fibre centralised parallel phantom\';
-file_dir = [file_dir 'x-05\'];
+file_dir = [file_dir 'x-05 c0_var t0_var\'];
 
-c0_sample = 1460:1:1500; % in m/s
+c0_sample = 1484:1:1494; % in m/s
+t0_sample = -18:1:-12;
+
 dist_lims = 2:0.1:10; % in mm
 
 c0_minnorms = zeros(1,length(dist_lims));
+t0_minnorms = zeros(1,length(dist_lims));
 
 for idx_dist = 1:length(dist_lims)
     
     dist_lim = dist_lims(idx_dist);
 
-    Norm = zeros(1,length(c0_sample));
-    Norm_std = zeros(1,length(c0_sample));
+    Norm = zeros(length(t0_sample),length(c0_sample));
+    Norm_std = zeros(length(t0_sample),length(c0_sample));
 
     for idx_c = 1:length(c0_sample)
+    for idx_t = 1:length(t0_sample)
 
         c0 = c0_sample(idx_c);
+        t0 = t0_sample(idx_t);
 
-        file_name = ['peaksInfoAll_x-05_c' sprintf('%0.1f',c0) '.mat'];
+        file_name = ['peaksInfoAll_x-05_c' sprintf('%0.1f',c0) '_t0_' num2str(t0) '.mat'];
         data = load([file_dir file_name]);
         peaksInfoAll = data.peaksInfoAll;
 
@@ -27,38 +32,46 @@ for idx_dist = 1:length(dist_lims)
         peaksPosZ    = peaksInfoAll(3,:)*1e3; % in mm
         peaksResoLat = peaksInfoAll(4,:)*1e6; % in um
 
-        includeInNorm = rectangularNorm(peaksPosX, peaksPosZ, dist_lim);
+        includeInNorm = circularNorm(peaksPosX, peaksPosZ, dist_lim);
         
         peaksResoLatInNorm = peaksResoLat(includeInNorm);
 
         % assert(length(peaksResoLatInNorm)==104);
-        Norm(idx_c) = mean(peaksResoLatInNorm);
-        Norm_std(idx_c) = std(peaksResoLatInNorm);
+        Norm(idx_t,idx_c) = mean(peaksResoLatInNorm);
+        Norm_std(idx_t,idx_c) = std(peaksResoLatInNorm);
 
+    end
     end
     
     figure
-    plot(c0_sample, Norm)
+    imagesc(c0_sample, t0_sample, Norm)
     hold on
-    plot(c0_sample, Norm - Norm_std, '--')
-    plot(c0_sample, Norm + Norm_std, '--')
+%     plot(c0_sample, Norm - Norm_std, '--')
+%     plot(c0_sample, Norm + Norm_std, '--')
     xlabel('c0 [m/s]')
-    ylabel('avg. lat. resolution within norm-radius [\mum]')
+    ylabel('t0 correction [dt]')
+%     ylabel('avg. lat. resolution within norm-radius [\mum]')
     title(['norm radius = ' num2str(dist_lim) ' mm'])
     set(gca,'FontSize',13)
-    axis([1460,1500,40,150])
+%     axis([1460,1500,40,150])
     drawnow
     pause(0.1)
 
-    [minnorm, idx_minnorm] = min(Norm);
-    c0_minnorms(idx_dist) = c0_sample(idx_minnorm);
+    minnorm = min(Norm,[],'all');
+    [idx_minnorm_t, idx_minnorm_c] = find(Norm==minnorm);
+    c0_minnorms(idx_dist) = c0_sample(idx_minnorm_c);
+    t0_minnorms(idx_dist) = t0_sample(idx_minnorm_t);
     
 end
 
 figure
+yyaxis left
 plot(dist_lims, c0_minnorms, 'x')
 xlabel('norm length scale [mm]')
 ylabel('c0 with minimum norm [m/s]')
+yyaxis right
+plot(dist_lims, t0_minnorms, 'o')
+ylabel('t0 correction with minimum norm [dt]')
 title('best c0 for different norm radii')
 set(gca,'FontSize',13)
 
