5786a59f745ece14cd7fa11cc7a5c880687d261f
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2020-02-19 11:01:21 +0000

    complete ustb test

--- simul_pwUS_ustb.m
+++ simul_pwUS_ustb.m
@@ -19,6 +19,61 @@ prb.plot(fig_handle);
 pul = uff.pulse();
 pul.center_frequency = 1e6;
 pul.fractional_bandwidth = 80;
-pul.plot([],'broadband pulse')
+pul.plot([],'broadband pulse');
 
 % sequency generation
+num_angle = 1;
+angles = 0;
+seq = uff.wave();
+for n = 1 : num_angle
+    seq(n) = uff.wave();
+    
+    seq(n).source.azimuth = angles(n);
+    seq(n).source.distance = Inf;
+    
+    seq(n).probe = prb;
+    
+    seq(n).sound_speed = pha.sound_speed;
+    
+    % show source
+    fig_handle  = seq(n).source.plot(fig_handle);
+end
+
+% simulator
+sim = fresnel();          % how to use field 2 ? do i want to use field 2 ?
+
+sim.phantom = pha;
+sim.pulse = pul;
+sim.probe = prb;
+sim.sequence = seq;
+sim.sampling_frequency = 40e6;
+
+channel_data = sim.go();
+
+% scan area
+sca = uff.linear_scan();
+sca.x_axis = linspace(-3e-3, 3e-3, 200).';
+sca.z_axis = linspace(3e-3, 7e-3, 200).';
+sca.plot(fig_handle, 'Scenario');
+
+% beamformer
+bmf = beamformer();
+bmf.channel_data = channel_data;
+bmf.scan = sca;
+
+bmf.receive_apodization.window = uff.window.tukey50;    % is receive apodized in our case ?
+bmf.receive_apodization.f_number = 1.7;                 % what is the f number ?
+bmf.receive_apodization.apex.distance = Inf;
+
+bmf.transmit_apodization.window = uff.window.tukey50;
+bmf.transmit_apodization.f_number = 1.7;                % what is the f number ?
+bmf.transmit_apodization.apex.distance = Inf;
+
+% beamforming
+b_data = bmf.go({ process.das_matlab() process.coherent_compounding() });
+
+b_data.plot()
+
+
+
+
