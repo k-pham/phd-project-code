56a005b64b7ce35b0ea023c51ff23223d66093b5
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2020-03-26 16:00:59 +0000

    test low pass 20 on atmm

--- upsampledRecons_paper.m
+++ upsampledRecons_paper.m
@@ -27,11 +27,11 @@ file_dir = '..\data\imagingUS\';
 %         file_name_cli = '180629\clinical scanner\29-06-2018_17-41-25 DICOM +2 higher gain\17-38-33.dcm';
 
 % 181204 atmm orgasol 1
-%     file_name = '181204/atmm_orgasol1_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_03s08m21h_04-12-18_avg1_2D_raw.SGL';
-%     trigger_delay = 0;
-%     samples_cut_off = 50;
-%     samples_t0_correct = -6;
-%     c0 = 1544;
+    file_name = '181204/atmm_orgasol1_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_03s08m21h_04-12-18_avg1_2D_raw.SGL';
+    trigger_delay = 0;
+    samples_cut_off = 50;
+    samples_t0_correct = -6;
+    c0 = 1544;
 
 % 180828 pork belly 3
 %     file_name = '180828\porkBelly3_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[20ns]_26s20m19h_28-08-18_avg1_2D_raw.SGL';
@@ -41,11 +41,11 @@ file_dir = '..\data\imagingUS\';
 %     c0 = 1460;
 
 % 190114 lymph node (L3)
-    file_name = '190114/lymphNode2_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_13s51m16h_14-01-19_avg1_2D_raw.SGL';
-    trigger_delay = 0;
-    samples_cut_off = 50;
-    samples_t0_correct = -6;
-    c0 = 1520;
+%     file_name = '190114/lymphNode2_BK31[CNT]@0nm_t0[0]_dx[100µm]_dy[100µm]_dt[8ns]_13s51m16h_14-01-19_avg1_2D_raw.SGL';
+%     trigger_delay = 0;
+%     samples_cut_off = 50;
+%     samples_t0_correct = -6;
+%     c0 = 1520;
 
 
 %% load sensor data
@@ -115,8 +115,8 @@ disp(['Reconstructing: ' file_name])
                             'ZeroPad', 10, ...
                             'Upsample', true, ...
                             'Apodise', false, ...
-                            'FreqBandFilter', {10e6, 15e6}, ...
-                            'FreqLowFilter', {}, ... % 30e6
+                            'FreqBandFilter', {}, ... % 10e6, 15e6
+                            'FreqLowFilter', {20e6}, ... % 30e6
                             'TimeGainCompensate', {}, ...
                             'EnvelopeDetect', true, ...
                             'LogCompress', 0, ...
