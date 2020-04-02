b7223e286dffa6ba72801c3c282a0cffcaada7e0
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2020-04-02 11:35:13 +0100

    change variable names and load water hole simulations

--- simul_scattTMM_2D_analysis.m
+++ simul_scattTMM_2D_analysis.m
@@ -5,17 +5,17 @@ file_dir_figs = 'D:\PROJECT\figures\_Matlab figs\simulations\scattTMM\';
 
 scattering_type = 'random';      % options: 'random', 'points'
 
-c_scatts   = 0:10:150;
-rho_scatts = 0:10:100;
+c_ranges   = 0:10:150;
+rho_ranges = 0:10:100;
 
 c_hole     = 1500;
-rho_hole   = 1300;
+rho_hole   = 1000;
 
 for idx_c = 16:16%length(c_scatts)
     for idx_r = 11:11%length(rho_scatts)
 
-        c_scatt = c_scatts(idx_c);
-        rho_scatt = rho_scatts(idx_r);
+        c_scatt = c_ranges(idx_c);
+        rho_scatt = rho_ranges(idx_r);
         
         file_name = [scattering_type '_SCATT_c' num2str(c_scatt) '_rho' num2str(rho_scatt) ...
                                      '_HOLE_c' num2str(c_hole) '_rho' num2str(rho_hole) ];
@@ -23,9 +23,9 @@ for idx_c = 16:16%length(c_scatts)
         SNS = load([file_dir_data file_name '_sensor_data.mat']);
         IMG = load([file_dir_data file_name '_image_data.mat']);
         
-        sensor_data = SNS.sensor_data;
-        params      = SNS.params;
-        simu        = SNS.simu;
+        sensor_data   = SNS.sensor_data;
+        sensor_params = SNS.params;
+        simu          = SNS.simu;
         
         image_data    = squeeze(IMG.volume_data);
         image_spacing = IMG.volume_spacing;
@@ -35,4 +35,23 @@ for idx_c = 16:16%length(c_scatts)
 
     end
 end
+
+
+%% LOCAL FUNCTIONS
+
+function look_at_central_sensor_data(sensor_data, params)
+
+    
+
+end
+
+function image_quality_metric = get_image_quality_metric(reflection_image)
+
+    
+
+end
+
+
+
+
 
