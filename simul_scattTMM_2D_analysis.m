a9d459af0e2a290fb6dbae91e0855a44f10c1c9a
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2020-04-16 14:49:15 +0100

    look at freq content of experimental data

--- simul_scattTMM_2D_analysis.m
+++ simul_scattTMM_2D_analysis.m
@@ -208,6 +208,9 @@ plot(exp.t_array*1e6, squeeze(exp.data(round(exp.params.Nx/2),round(exp.params.N
     xlim([0,10])
     ylim([-0.3,0.2])
 
+[frequency, f_series] = spect(squeeze(exp.data(75,75,:)),1/exp.params.dt); % ,'Window','Tukey'
+figure, plot(frequency/1e6,f_series,'b')
+
 
 %% LOCAL FUNCTIONS
 
