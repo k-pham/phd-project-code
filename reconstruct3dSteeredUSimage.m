d0abc36cbcbaf697bb0883226383e27f32e72a0f
Khoa Pham <khoa.pham.16@ucl.ac.uk>
2021-07-20 23:24:07 +0100

    find TOA map and fit planar wave to TOA

--- reconstruct3dSteeredUSimage.m
+++ reconstruct3dSteeredUSimage.m
@@ -23,6 +23,66 @@ plot(squeeze(sensor_data(70,70,:)))
 
 %% fit plane wave to source
 
+% note peaks will be saturated, so for TOA should pick first one at max val
+
+% get PMAX and TOA in 2d grid and vector
+[ PMAX_xy, TOA_xy ] = max(sensor_data,[],3);  % if max element occurs more than once, then TOA contains index to first occurrence
+[Nx,Ny] = size(TOA_xy);
+x = 1:Nx;
+y = 1:Ny;
+[X,Y] = meshgrid(x,y);X=X';Y=Y';
+Xv = reshape(X,[Nx*Ny 1]);
+Yv = reshape(Y,[Nx*Ny 1]);
+PMAX_vec = reshape(PMAX_xy,[Nx*Ny 1]);
+TOA_vec  = reshape(TOA_xy, [Nx*Ny 1]);
+
+% may need to apply mask for central area to avoid outliers at edge ruining fit
+
+% % apply mask to data to use for regression
+% TOA_vec_mask = TOA_xy(mask);
+% Xv_mask = Xv(mask);
+% Yv_mask = Yv(mask);
+TOA_vec_mask = TOA_vec;
+Xv_mask = Xv;
+Yv_mask = Yv;
+
+% regression in 2d TOA(x,y) = p1 + p2*x + p3*y using masked data points
+Model = [ones(length(Xv_mask),1) Xv_mask Yv_mask];
+params = Model \ TOA_vec_mask;
+
+% evaluate fit over whole scanning area
+TOA_fit_vec = params(1) + params(2) * Xv + params(3) * Yv;
+TOA_fit_xy = reshape(TOA_fit_vec,[Nx Ny]);
+
+% get deviation in time index of arrival from fit within mask
+TOA_fit_vec_mask = TOA_fit_vec;%(mask);
+deviation_mask = TOA_vec_mask - TOA_fit_vec_mask;
+
+% plot deviation of TOA from TOA_fit in 2d colour scatter
+figure
+scatter(Xv_mask,Yv_mask,[],deviation_mask,'o')
+hold on
+    colorbar
+    %caxis([-3,3])
+    title('time index of arrival: deviation from fit within ROI')
+    xlabel('x')
+    ylabel('y')
+    %xlim([30,120])
+    %ylim([20,120])
+    axis equal
+    
+% plot 3d scatter of TOA for whole scanning area with fit from masked data
+figure
+scatter3(Xv,Yv,TOA_vec,'.')
+hold on
+mesh(X,Y,TOA_fit_xy)
+    title('Time index of arrival for each interrogation point with fit')
+    xlabel('x')
+    ylabel('y')
+    zlabel('time index of arrival')
+    xlim([0,Nx])
+    ylim([0,Ny])
+    %zlim([160,180])
 
 
 %% calculate steering angle (a), rotation angle (b) & time offset (t0)
